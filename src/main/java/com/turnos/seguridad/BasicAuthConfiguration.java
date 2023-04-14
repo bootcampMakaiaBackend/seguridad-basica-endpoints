190d35d2405094195836b6c2381875feb19a7ca3
mateo <mateozacar@gmail.com>
2023-04-14 08:13:56 -0500

    spring security

--- src/main/java/com/turnos/seguridad/BasicAuthConfiguration.java
+++ src/main/java/com/turnos/seguridad/BasicAuthConfiguration.java
@@ -3,9 +3,13 @@ package com.turnos.seguridad;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -21,31 +25,43 @@ public class BasicAuthConfiguration {
     private String password;
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-        UserDetails user = User
-                .withUsername(username)
-                .password(passwordEncoder().encode(password))
-                .roles("READ")
-                .build();
-        return new InMemoryUserDetailsManager(user);
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests()
-                .antMatchers("/public").permitAll()
-                .anyRequest().authenticated()
+    SecurityFilterChain filterChain(HttpSecurity http,
+                                    AuthenticationManager authManager) throws Exception {
+        return http.csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
                 .and()
-                .httpBasic();
-        return httpSecurity.build();
+                .httpBasic()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .build();
     }
-
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder(8);
+    UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername(username)
+                .password(passwordEncoder().encode(password))
+                .roles()
+                .build()
+        );
+        return manager;
     }
 
+    @Bean
+    AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService())
+                .passwordEncoder(passwordEncoder())
+                .and().build();
+    }
 
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
 }
